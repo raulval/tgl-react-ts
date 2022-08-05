b4920afadb0562cbc96609619744f014bf417e2e
Raul Val <raulval@outlook.com>
2022-08-05 11:58:47 -0300

    fix: persist data in local storage

--- front-end/src/Router.tsx
+++ front-end/src/Router.tsx
@@ -1,24 +1,27 @@
 import { Provider } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import { PersistGate } from "redux-persist/integration/react";
 import Bet from "./pages/Bet";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Reset from "./pages/Reset";
 import SignUp from "./pages/SignUp";
-import store from "./store";
+import { persistor, store } from "./store";
 
 const Router = () => {
   return (
     <Provider store={store}>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/reset" element={<Reset />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/bet" element={<Bet />} />
-      </Routes>
-      <ToastContainer limit={3} />
+      <PersistGate loading={null} persistor={persistor}>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/reset" element={<Reset />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/bet" element={<Bet />} />
+        </Routes>
+        <ToastContainer limit={3} />
+      </PersistGate>
     </Provider>
   );
 };
--- front-end/src/store/index.ts
+++ front-end/src/store/index.ts
@@ -1,14 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import betsReducer from "./betSlice";
 import gamesReducer from "./gameSlice";
 import userReducer from "./userSlice";
 
-export const store = configureStore({
-  reducer: {
-    user: userReducer,
-    games: gamesReducer,
-    bets: betsReducer,
-  },
+const reducers = combineReducers({
+  user: userReducer,
+  games: gamesReducer,
+  bets: betsReducer,
 });
 
-export default store;
+const persistConfig = {
+  key: "TGL",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+const persistor = persistStore(store);
+
+export { store, persistor };
