771b36de0132333c7ab7ba00c7ba552a3dac320d
Raul Val <raulval@outlook.com>
2023-09-06 16:44:51 -0300

    ➕ add react query

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,13 +1,18 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "./Router";
 import { GlobalStyle } from "./styles/global";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-      <Router />
-      <GlobalStyle />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
