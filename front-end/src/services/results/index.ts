07219d998f167461cc1e9cc0e5ade62944108b3f
Raul Val <raulval@outlook.com>
2023-09-23 14:55:12 -0300

    ✨ add endpoint to get sport results

--- front-end/src/services/results/index.ts
+++ front-end/src/services/results/index.ts
@@ -1,19 +1,35 @@
 import { api } from "services/api";
 import { useQuery } from "@tanstack/react-query";
-import { ILotteryResultsResponse } from "./types";
+import { ILotteryResultsResponse, ISportResultsResponse } from "./types";
 
 const API_ENDPOINTS = {
   lotteryResults: (lottery: string) => `results/lottery/${lottery}`,
+  getSportResults: (league: string) => `results/sports/${league}`,
 };
 
 const useGetLotteryResults = (lottery: string) => {
-  return useQuery<ILotteryResultsResponse, Error>(
-    ["lottery-results"],
-    async () => {
+  return useQuery<ILotteryResultsResponse, Error>({
+    queryKey: ["lottery-results"],
+    queryFn: async () => {
       const { data } = await api.get(API_ENDPOINTS.lotteryResults(lottery));
       return data;
-    }
-  );
+    },
+    refetchOnWindowFocus: false,
+  });
 };
 
-export { useGetLotteryResults };
+const useGetSportResults = (league: string) => {
+  return useQuery<ISportResultsResponse, Error>({
+    queryKey: ["sport-results"],
+    queryFn: async () => {
+      if (league === "") {
+        return null;
+      }
+      const { data } = await api.get(API_ENDPOINTS.getSportResults(league));
+      return data;
+    },
+    refetchOnWindowFocus: false,
+  });
+};
+
+export { useGetLotteryResults, useGetSportResults };
--- front-end/src/services/results/types.ts
+++ front-end/src/services/results/types.ts
@@ -1,3 +1,5 @@
+import { IMatch, IParticipants } from "services/sports/types";
+
 export interface ILotteryResultsResponse {
   name: string;
   date: string;
@@ -12,4 +14,41 @@ export interface Prize {
   scores: string;
   winners: number;
   prize: string;
+}
+
+export type ISportResultsResponse = ISportResult[];
+
+export interface ISportResult {
+  result: IResult;
+  sportBet?: ISportBet;
+}
+
+export interface IResult {
+  id: number;
+  winner: string;
+  score: IScore;
+  participants: IParticipants;
+  started_date: number;
+  match_id: string;
+  created_at: string;
+  updated_at: string;
+  league: string;
+  match: IMatch;
+}
+
+export interface IScore {
+  home: number;
+  guest: number;
+}
+
+export interface ISportBet {
+  id: number;
+  match_id: string;
+  user_id: number;
+  picked: string;
+  odd: number;
+  amount: number;
+  earning: number;
+  created_at: string;
+  updated_at: string;
 }
